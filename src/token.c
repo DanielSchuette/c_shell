9f751c0715e2009d95116a23e433b27b12957c43
DanielSchuette <d.schuette@online.de>
2019-04-09 23:27:15 +0200

    add single and double quote support

--- src/token.c
+++ src/token.c
@@ -1,19 +1,68 @@
+#include <stdio.h>
 #include <string.h>
 
 #include "token.h"
+#include "utils.h"
 
 char *get_token(char **line)
 {
     char *token;
-    while ((token = strsep(line, DELIMS)) && (strcmp(token, "") == 0))
-        ; /* return if token is NULL or anything but "" */
+    char quote;
+    int llen;
+    static char buf[BUF_SIZE];
 
-    /*
-     * TODO: does token start with \' or \"? If it does, split off quote and
-     * get tokens until a matching quote is reached. Then, return the entire
-     * string which was enclosed in the quotes without the quotes but with all
-     * white space.
-     */
+    /* TODO: replace with a custom function, so DELIMS aren't lost */
+    while ((token = strsep(line, DELIMS)) && (strcmp(token, "") == 0))
+        ; /* loop until token is NULL or anything but "" */
+
+    /* TODO: refactor into separate function */
+    /* handle tokens that are enclosed in single or double quotes */
+    if (token && (*token == '\'' || *token == '"')) {
+        llen  = strlen(*line);
+        quote = *token; /* save quote for later comparisons */
+
+        /* check if only a single token is quoted without DELIMS */
+        if (token[strlen(token) - 1] == quote) {
+            token[strlen(token) - 1] = '\0'; /* strip quotes off */
+            return &token[1];                /* and return token */
+        } else if (llen == 0) {
+            /* no more chars on line, but also no closing quote */
+            fprintf(stderr, "Error: No closing quote `%c`.", quote);
+            return NULL;
+        }
+
+        /*
+         * If potentially multiple tokens are enclosed in quotes,
+         * an empty buffer is used, the first token is copied into
+         * it (without the leading quote) and all chars up to the
+         * closing quote are added, too. Since the first separator
+         * is lost to the tokenizing function `strsep', a whitespace
+         * is added for now.
+         */
+        if (strlen(token) < BUF_SIZE - 1) {
+            strncpy(buf, &token[1], BUF_SIZE);
+            int _len      = strlen(buf);
+            buf[_len]     = ' ';  /* ' ' was stripped off by */
+            buf[_len + 1] = '\0'; /* strsep and is lost */
+        }
+
+        while (**line != quote && --llen > 0) {
+            int _len = strlen(buf);
+            if (_len < BUF_SIZE - 1) {
+                buf[_len]     = **line;
+                buf[_len + 1] = '\0';
+            } else {
+                fprintf(stderr, "Error: Quoted string too long.");
+            }
+            (*line)++;
+        }
+
+        if (llen == 0) {
+            fprintf(stderr, "Error: No closing quote `%c`.", quote);
+            return NULL; /* no valid token found */
+        }
+        token = buf;
+    }
 
     return token;
 }
